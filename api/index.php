4551b73653994efb1b90069673afa16650c832fc
moviemil <134944648+moviemil@users.noreply.github.com>
2024-01-09 07:35:23 -0300

    index.php

--- api/index.php
+++ api/index.php
@@ -7,24 +7,24 @@ curl_setopt($ch, CURLOPT_URL, $feedURL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 if ($response === false) {
-echo 'Curl error: ' . curl_error($ch);
+  echo 'Curl error: ' . curl_error($ch);
 } else {
-$json = json_decode($response, true);
-$conteudo = $json['feed']['entry'][0]['content']['$t'];
-if (isset($_GET['d'])) {
-$dadosBase64 = explode(',', $conteudo, 2)[1];
-preg_match('/^data:[^\/]+\/([a-zA-Z]+);base64/', $conteudo, $matches);
-$tipoConteudo = isset($matches[1]) ? $matches[1] : 'octet-stream';
-$headerContentType = "Content-type: $tipoConteudo";
-header($headerContentType);
-$extensao = $tipoConteudo;
-header("Content-Disposition: attachment; filename=$title.$extensao");
-echo base64_decode($dadosBase64);
-} else {
-$tipoConteudo = mime_content_type($conteudo);
-header("Content-type: $tipoConteudo");
-readfile($conteudo);
-}
+  $json = json_decode($response, true);
+  $conteudo = $json['feed']['entry'][0]['content']['$t'];
+  if (isset($_GET['d'])) {
+    $dadosBase64 = explode(',', $conteudo, 2)[1];
+    preg_match('/^data:[^\/]+\/([a-zA-Z]+);base64/', $conteudo, $matches);
+    $tipoConteudo = isset($matches[1]) ? $matches[1] : 'octet-stream';
+    $headerContentType = "Content-type: $tipoConteudo";
+    header($headerContentType);
+    $extensao = $tipoConteudo;
+    header("Content-Disposition: attachment; filename=$title.$extensao");
+    echo base64_decode($dadosBase64);
+  } else {
+    $tipoConteudo = mime_content_type($conteudo);
+    header("Content-type: $tipoConteudo");
+    readfile($conteudo);
+  }
 }
 curl_close($ch);
 ?>
