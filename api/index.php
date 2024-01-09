209b972284fde8777957e6c7a27e5e070de69197
moviemil <134944648+moviemil@users.noreply.github.com>
2024-01-09 07:27:40 -0300

    index.php

--- api/index.php
+++ api/index.php
@@ -6,15 +6,12 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $feedURL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
-
 if ($response === false) {
-    echo 'Curl error: ' . curl_error($ch);
+echo 'Curl error: ' . curl_error($ch);
 } else {
-    $json = json_decode($response, true);
-    
-    $conteudo = $json['feed']['entry'][0]['content']['$t'];
-
-    if (isset($_GET['d'])) {
+$json = json_decode($response, true);
+$conteudo = $json['feed']['entry'][0]['content']['$t'];
+if (isset($_GET['d'])) {
 $dadosBase64 = explode(',', $conteudo, 2)[1];
 preg_match('/^data:[^\/]+\/([a-zA-Z]+);base64/', $conteudo, $matches);
 $tipoConteudo = isset($matches[1]) ? $matches[1] : 'octet-stream';
@@ -23,12 +20,11 @@ header($headerContentType);
 $extensao = $tipoConteudo;
 header("Content-Disposition: attachment; filename=$title.$extensao");
 echo base64_decode($dadosBase64);
-    } else {
-        $tipoConteudo = mime_content_type($conteudo);
-        header("Content-type: $tipoConteudo");
-        readfile($conteudo);
-    }
+} else {
+$tipoConteudo = mime_content_type($conteudo);
+header("Content-type: $tipoConteudo");
+readfile($conteudo);
 }
-
+}
 curl_close($ch);
 ?>
